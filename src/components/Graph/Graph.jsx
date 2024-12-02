9b9ed0e7c1c8db16c0387680c69e2dd99b87ce93
Timur Stupin <hache03@gmail.com>
2024-12-02 04:34:15 +0300

    desmos->svg

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,115 +1,151 @@
-import {forwardRef, useEffect, useImperativeHandle, useRef} from "react";
+import {forwardRef, Fragment, useEffect, useImperativeHandle, useRef, useState} from "react";
 import styles from "./Graph.module.css";
 import PropTypes from "prop-types";
 
 const Graph = forwardRef(({pointChecker, radius, history}, ref) => {
-    const graphContainerRef = useRef(null);
-    const graphRef = useRef(null);
+    const svgRef = useRef(null);
     const isDataLoaded = useRef(false);
 
-    function initGraph() {
-        graphRef.current = window.Desmos.GraphingCalculator(graphContainerRef.current, {
-            keypad: false,
-            expressions: false,
-            settingsMenu: false,
-            lockViewport: true,
-            zoomFit: true,
-            pointsOfInterest: false,
-            trace: false,
-            xAxisStep: 1,
-            yAxisStep: 1,
-            showGrid: false
-        });
+    function handleRChange() {
+        setRSerif(() => radius ? radius : "R");
+        setRDiv2Serif(() => radius ? radius / 2 : "R/2");
+        redrawPoints();
     }
 
-    function drawGraph(r) {
-        graphRef.current.setExpression({
-            id: "area1",
-            latex: `x^{2}+y^{2}<=${r}^2\\{y>=0\\}\\{x>=0\\}`,
-            color: window.Desmos.Colors.BLUE
-        });
-        graphRef.current.setExpression({
-            id: "area2",
-            latex: `y<=2x+${r}\\{y>=0\\}\\{x<=0\\}`,
-            color: window.Desmos.Colors.BLUE
-        });
-        graphRef.current.setExpression({
-            id: "area3",
-            latex: `-${r}<=x<=0\\{-${r}/2<=y<=0\\}`,
-            color: window.Desmos.Colors.BLUE
-        });
-        graphRef.current.setExpression({
-            id: "line1",
-            latex: `y=0\\{-${r}<=x<=-${r}/2\\}`,
-            color: window.Desmos.Colors.BLUE
-        });
-        graphRef.current.setExpression({
-            id: "line2",
-            latex: `y=-${r}/2\\{-${r}<=x<=0\\}`,
-            color: window.Desmos.Colors.BLUE
-        });
-        graphRef.current.setExpression({
-            id: "line3",
-            latex: `y=0\\{0<=x<=${r}\\}`,
-            color: window.Desmos.Colors.BLUE
-        });
-        graphRef.current.setMathBounds({
-            left: -r - 1,
-            right: r + 1,
-            bottom: -r - 1,
-            top: r + 1
-        });
-    }
+    //TODO: remove hardcode
+    const length = 350;
 
-    function destroyGraph() {
-        if (graphRef.current) {
-            graphRef.current.destroy();
-        }
-    }
+    useEffect(handleRChange, [radius]);
+
+    const [RSerif, setRSerif] = useState("R");
+    const [RDiv2Serif, setRDiv2Serif] = useState("R/2");
+
+    const half = length / 2;
+    const seg = half / 3;
+    const mcr = seg / 10;
+    const tick = mcr / 2;
+
+    const axisData = [{x1: -half, y1: 0, x2: half, y2: 0}, {x1: 0, y1: half, x2: 0, y2: -half}];
+    const arrowData = [{x: half, y: 0, dx: -2 * mcr, dy: mcr, c: -1}, {x: 0, y: -half, dx: mcr, dy: 2 * mcr, c: 1}];
+    const serifData = [-2 * seg, -seg, seg, 2 * seg];
+    const labelData = [{c: 'X', dx: 0, dy: 4 * mcr, cx: 1, cy: 0}, {c: 'Y', dx: mcr, dy: mcr, cx: 0, cy: 1}];
+    const labelValues = ['-' + RSerif, '-' + RDiv2Serif, RDiv2Serif, RSerif];
 
     function handleGraphClick(event) {
-        let calculatorRect = graphContainerRef.current.getBoundingClientRect();
-        let {x, y} = graphRef.current.pixelsToMath({
-            x: event.clientX - calculatorRect.left,
-            y: event.clientY - calculatorRect.top
-        });
-        pointChecker(x, y, radius);
+        const plane = svgRef.current;
+
+        let point = plane.createSVGPoint();
+
+        point.x = event.clientX;
+        point.y = event.clientY;
+
+        point = point.matrixTransform(plane.getScreenCTM().inverse());
+
+        const scale = radius / (2 * seg);
+
+        point.x *= scale;
+        point.y *= -scale;
+        pointChecker(point.x, point.y, radius);
     }
 
     function drawPoint(x, y, hit) {
-        if (hit !== null) {
-            graphRef.current.setExpression({
-                latex: `(${x}, ${y})`,
-                color: hit ? "green" : "red"
-            });
+        const svg = svgRef.current;
+        if (hit !== null && svg) {
+            const scale = radius / (2 * seg);
+
+            let graphX = x / scale;
+            let graphY = y / -scale;
+
+            const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+            circle.setAttribute("cx", graphX);
+            circle.setAttribute("cy", graphY);
+            circle.setAttribute("class", styles["point"]);
+            circle.setAttribute("fill", hit ? "green" : "red");
+            svg.querySelector("#points").appendChild(circle);
         }
     }
 
-    useEffect(() => {
-        if (window.Desmos) initGraph();
-        return destroyGraph;
-    }, []);
+    function removeAllPoints() {
+        const points = svgRef.current.querySelectorAll("circle");
+        points.forEach(point => point.remove());
+    }
 
-    useEffect(() => {
-        if (isDataLoaded.current || history.length === 0) return;
+    function redrawPoints() {
+        removeAllPoints();
         history.forEach((point) => {
             drawPoint(point.x, point.y, point.hit);
         });
-        isDataLoaded.current = true;
-    }, [history]);
+    }
 
     useEffect(() => {
-        drawGraph(radius);
-    }, [radius]);
+        if (isDataLoaded.current || history.length === 0) return;
+        redrawPoints();
+        isDataLoaded.current = true;
+    }, [history]);
 
     useImperativeHandle(ref, () => ({drawPoint}));
 
     return (
-        <div
-            className={styles["graph-container"]}
-            ref={graphContainerRef}
-            onClick={handleGraphClick}
-        ></div>
+        <div className={styles["graph-container"]}>
+            <svg
+                className={styles["graph"]}
+                ref={svgRef}
+                viewBox={`${-half} ${-half} ${length} ${length}`}
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={handleGraphClick}
+            >
+                <g id="graph">
+                    <path
+                        d={`M 0 0 0 ${-2 * seg} A ${2 * seg} ${2 * seg} 0 0 1 ${2 * seg} 0 L ${seg} 0 ${seg} ${2 * seg} 0 ${2 * seg} 0 0 ${-seg} 0 0 ${-2 * seg} Z`}
+                        className={styles["figure"]}/>
+                </g>
+                <g id="axis">
+                    {axisData.map(({x1, y1, x2, y2}, i) => (
+                        <line key={`axis-${i}`}
+                              x1={x1} y1={y1}
+                              x2={x2} y2={y2}
+                              className={styles["axis"]}/>
+
+                    ))}
+
+                    {arrowData.map(({x, y, dx, dy, c}, i) => (
+                        <path key={`arrow-${i}`}
+                              d={`M ${x + dx} ${y + dy} L ${x} ${y} L ${x - c * dx} ${y + c * dy}`}
+                              className={styles["arrow"]}/>
+
+                    ))}
+
+                    {serifData.map((c, i) =>
+                        [[c, tick, 1], [tick, c, -1]].map(([x, y, cnt], j) => (
+                            <line key={`serif-${i}-${j}`}
+                                  x1={x} y1={y}
+                                  x2={cnt * x} y2={-cnt * y}
+                                  className={styles["axis"]}/>
+
+                        ))
+                    )}
+                </g>
+                <g id="points"/>
+                <g id="labels">
+                    {labelData.map(({c, dx, dy, cx, cy}, i) => (
+                        <Fragment key={`label-container-${i}`}>
+                            <text key={`label-${i}`}
+                                  x={cx * half - 2 * dx}
+                                  y={dy - cy * (half - dy)}
+                                  textAnchor="end"
+                                  className={styles["label"]}>{c}</text>
+                            {labelValues.map((val, j) => (
+                                <text key={`label-${i}-${j}`}
+                                      x={cx * serifData[j] - dx}
+                                      y={dy - cy * serifData[j]}
+                                      textAnchor={cx ? "middle" : "end"}
+                                      className={styles["label"]}>{val}</text>
+                            ))}
+                        </Fragment>
+                    ))}
+                </g>
+            </svg>
+        </div>
     );
 });
 
